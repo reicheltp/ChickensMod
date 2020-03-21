365602aced70fd57e099317183a77df734f0e513
Raphael Zöllner <raphael.zoellner@outlook.com>
2020-03-21 01:39:57 +0100

    Update BlockHenhouse

--- src/main/java/com/setycz/chickens/block/BlockHenhouse.java
+++ src/main/java/com/setycz/chickens/block/BlockHenhouse.java
@@ -2,76 +2,77 @@ package com.setycz.chickens.block;
 
 import java.util.List;
 
-import com.setycz.chickens.ChickensMod;
-
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ContainerBlock;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.translation.I18n;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentUtils;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.network.NetworkHooks;
+
+import javax.annotation.Nullable;
 
 /**
  * Created by setyc on 01.03.2016.
  */
 public class BlockHenhouse extends ContainerBlock {
-    public static final PropertyDirection FACING = PropertyDirection.create("facing", Direction.Plane.HORIZONTAL);
+    public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
 
     public BlockHenhouse() {
-        super(Material.WOOD);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, Direction.NORTH));
-        this.setHardness(2);
-    }
-    
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
-    	super.addInformation(stack, player, tooltip, advanced);
-        tooltip.add(I18n.translateToLocal("tile.henhouse.tooltip"));
+        super(Properties.create(Material.WOOD).hardnessAndResistance(2));
+        this.getDefaultState().with(FACING, Direction.NORTH);
     }
 
     @Override
-    public TileEntity createNewTileEntity(World worldIn, int meta) {
+    @OnlyIn(Dist.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable IBlockReader blockReader, List<ITextComponent> tooltip, ITooltipFlag advanced) {
+        super.addInformation(stack, blockReader, tooltip, advanced);
+        tooltip.add(TextComponentUtils.toTextComponent(() -> I18n.format("tile.henhouse.tooltip")));
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(IBlockReader iBlockReader) {
         return new TileEntityHenhouse();
     }
 
     @Override
-    public void breakBlock(World worldIn, BlockPos pos, BlockState state) {
-        TileEntity tileEntity = worldIn.getTileEntity(pos);
+    public void onReplaced(BlockState oldBlockState, World world, BlockPos blockPos, BlockState newBlockState, boolean p_196243_5_) {
+        TileEntity tileEntity = world.getTileEntity(blockPos);
         if (tileEntity instanceof TileEntityHenhouse) {
             ((TileEntityHenhouse) tileEntity).dropContents();
         }
 
-        super.breakBlock(worldIn, pos, state);
+        super.onReplaced(oldBlockState, world, blockPos, newBlockState, p_196243_5_);
     }
 
     @Override
     public BlockState getStateForPlacement(World world, BlockPos pos, Direction facing, float hitX, float hitY, float hitZ, int meta, LivingEntity placer, Hand hand) {
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+        return this.getDefaultState().with(FACING, placer.getHorizontalFacing().getOpposite());
     }
 
     @Override
     public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
-        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+        worldIn.setBlockState(pos, state.with(FACING, placer.getHorizontalFacing().getOpposite()), 2);
 
         if (stack.hasDisplayName()) {
             TileEntity tileEntity = worldIn.getTileEntity(pos);
             if (tileEntity instanceof TileEntityHenhouse) {
-                ((TileEntityHenhouse) tileEntity).setCustomName(stack.getDisplayName());
+                ((TileEntityHenhouse) tileEntity).setCustomName(stack.getDisplayName().getFormattedText());
             }
         }
     }
@@ -83,44 +84,24 @@ public class BlockHenhouse extends ContainerBlock {
 
     @Override
     @Deprecated
-    public BlockState getStateFromMeta(int meta) {
-        Direction enumfacing = Direction.byIndex(meta);
-        if (enumfacing.getAxis() == Direction.Axis.Y) {
-            enumfacing = Direction.NORTH;
-        }
-        return getDefaultState().withProperty(FACING, enumfacing);
-    }
-
-    @Override
-    public int getMetaFromState(BlockState state) {
-        return state.getValue(FACING).getIndex();
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
     }
 
     @Override
     @Deprecated
-    public BlockState withRotation(BlockState state, Rotation rot) {
-        return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(FACING)));
     }
 
     @Override
-    @Deprecated
-    public BlockState withMirror(BlockState state, Mirror mirrorIn) {
-        return state.withRotation(mirrorIn.toRotation(state.getValue(FACING)));
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING);
-    }
-
-    @Override
-    public boolean onBlockActivated(World worldIn, BlockPos pos, BlockState state, PlayerEntity playerIn, Hand hand, Direction side, float hitX, float hitY, float hitZ) {
+    public ActionResultType func_225533_a_(BlockState state, World worldIn, BlockPos pos, PlayerEntity playerIn, Hand hand, BlockRayTraceResult hit) {
         if (!worldIn.isRemote) {
             TileEntity tileEntity = worldIn.getTileEntity(pos);
             if (tileEntity instanceof TileEntityHenhouse) {
-                playerIn.openGui(ChickensMod.instance, 0, worldIn, pos.getX(), pos.getY(), pos.getZ());
+                NetworkHooks.openGui((ServerPlayerEntity) playerIn, getContainer(state, worldIn, pos), pos);
             }
         }
-        return true;
+        return ActionResultType.PASS;
     }
 }
