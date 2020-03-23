ff86ce1a6068deb8fc095f64611613243e98c156
Raphael Zöllner <raphael.zoellner@outlook.com>
2020-03-23 23:03:49 +0100

    WIP

--- src/main/java/com/setycz/chickens/item/ItemSpawnEgg.java
+++ src/main/java/com/setycz/chickens/item/ItemSpawnEgg.java
@@ -11,27 +11,21 @@ import com.setycz.chickens.registry.ChickensRegistryItem;
 import init.ModItemGroups;
 import init.ModItems;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityList;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponent;
 import net.minecraft.util.text.TextComponentUtils;
-import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
-
-import java.util.List;
 
 /**
  * Created by setyc on 12.02.2016.
@@ -77,13 +71,18 @@ public class ItemSpawnEgg extends Item implements IColorSource {
         return renderPass == 0 ? chickenDescription.getBgColor() : chickenDescription.getFgColor();
     }
 
+
+
     @Override
-    public ActionResultType onItemUse(PlayerEntity playerIn, World worldIn, BlockPos pos, Hand hand, Direction facing, float hitX, float hitY, float hitZ) {
+    public ActionResultType onItemUse(ItemUseContext context) {
+        PlayerEntity playerIn = context.getPlayer();
+        World worldIn = context.getWorld();
+
         if (!worldIn.isRemote) {
-            ItemStack stack = playerIn.getHeldItem(hand);
-            BlockPos correlatedPos = correctPosition(pos, facing);
-            activate(stack, worldIn, correlatedPos, stack.getMetadata());
-            if (!playerIn.capabilities.isCreativeMode) {
+            ItemStack stack = playerIn.getHeldItem(context.getHand());
+            BlockPos correlatedPos = correctPosition(context.getPos(), context.getFace());
+            activate(stack, worldIn, correlatedPos);
+            if (!playerIn.isCreative()) {
                 stack.shrink(1);
             }
         }
@@ -102,34 +101,36 @@ public class ItemSpawnEgg extends Item implements IColorSource {
         return new BlockPos(posX, posY, posZ);
     }
 
-    private void activate(ItemStack stack, World worldIn, BlockPos pos, int metadata) {
-        ResourceLocation entityName = new ResourceLocation(ChickensMod.MODID, ChickensMod.CHICKEN);
-        EntityChickensChicken entity = (EntityChickensChicken) EntityList.createEntityByIDFromName(entityName, worldIn);
+    private void activate(ItemStack stack, World worldIn, BlockPos pos) {
+        EntityChickensChicken entity = new EntityChickensChicken(worldIn);
         if (entity == null) {
             return;
         }
-        entity.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+
         entity.onInitialSpawn(worldIn.getDifficultyForLocation(pos), null);
         entity.setChickenType(getTypeFromStack(stack));
 
-        CompoundNBT stackNBT = stack.getTagCompound();
+        CompoundNBT stackNBT = stack.serializeNBT();
+
         if (stackNBT != null) {
-            CompoundNBT entityNBT = entity.writeToNBT(new CompoundNBT());
+            entity.writeEntityToNBT(new CompoundNBT());
+            CompoundNBT entityNBT = entity.serializeNBT();
             entityNBT.merge(stackNBT);
             entity.readEntityFromNBT(entityNBT);
         }
 
-        worldIn.spawnEntity(entity);
+        BlockPos spawnPos = new BlockPos(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+        entity.getType().spawn(worldIn, null, null, spawnPos, SpawnReason.MOB_SUMMONED, true, false);
     }
     
     
     public static void applyEntityIdToItemStack(ItemStack stack, ResourceLocation entityId)
     {
-        CompoundNBT nbttagcompound = stack.hasTagCompound() ? stack.getTagCompound() : new CompoundNBT();
+        CompoundNBT nbttagcompound = stack.hasTag() ? stack.getTag() : new CompoundNBT();
         CompoundNBT nbttagcompound1 = new CompoundNBT();
-        nbttagcompound1.setString("id", entityId.toString());
-        nbttagcompound.setTag("ChickenType", nbttagcompound1);
-        stack.setTagCompound(nbttagcompound);
+        nbttagcompound1.putString("id", entityId.toString());
+        nbttagcompound.put("ChickenType", nbttagcompound1);
+        stack.setTag(nbttagcompound);
     }
     
     
@@ -140,12 +141,12 @@ public class ItemSpawnEgg extends Item implements IColorSource {
     @Nullable
     public static String getTypeFromStack(ItemStack stack)
     {
-    	CompoundNBT nbttagcompound = stack.getTagCompound();
+    	CompoundNBT nbttagcompound = stack.getTag();
 
-    	if (nbttagcompound != null && nbttagcompound.hasKey("ChickenType", 10))
+    	if (nbttagcompound != null && nbttagcompound.contains("ChickenType", 10))
     	{
     		new CompoundNBT();
-    		CompoundNBT chickentag = nbttagcompound.getCompoundTag("ChickenType");
+    		CompoundNBT chickentag = nbttagcompound.getCompound("ChickenType");
     		
     		return chickentag.getString("id");
     	}
