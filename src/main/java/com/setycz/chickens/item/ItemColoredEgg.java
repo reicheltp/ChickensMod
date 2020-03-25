36ea4cdfc0be358a7dd48a7d9b8fbdc5e40932c7
Raphael Zöllner <raphael.zoellner@outlook.com>
2020-03-25 22:52:34 +0100

    WIP

--- src/main/java/com/setycz/chickens/item/ItemColoredEgg.java
+++ src/main/java/com/setycz/chickens/item/ItemColoredEgg.java
@@ -1,6 +1,7 @@
 package com.setycz.chickens.item;
 
 import java.util.List;
+import java.util.Random;
 
 import com.setycz.chickens.ChickensMod;
 import com.setycz.chickens.entity.EntityColoredEgg;
@@ -9,18 +10,21 @@ import com.setycz.chickens.registry.ChickensRegistry;
 import com.setycz.chickens.registry.ChickensRegistryItem;
 
 import init.ModItemGroups;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.EggItem;
 import net.minecraft.util.*;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.Stats;
-import net.minecraft.util.text.translation.I18n;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentUtils;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
 
 /**
  * Created by setyc on 13.02.2016.
@@ -36,22 +40,22 @@ public class ItemColoredEgg extends EggItem implements IColorSource {
         return "colored_egg";
     }
 
-    @SideOnly(Side.CLIENT)
+    @OnlyIn(Dist.CLIENT)
     @Override
-    public void addInformation(ItemStack stack,  World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
-        tooltip.add(I18n.translateToLocal("item.colored_egg.tooltip"));
+        tooltip.add(TextComponentUtils.toTextComponent(() -> I18n.format("item.colored_egg.tooltip")));
     }
 
     @Override
-    public String getItemStackDisplayName(ItemStack stack) {
+    public ITextComponent getDisplayName(ItemStack stack) {
         DyeColor color = DyeColor.byDyeDamage(stack.getMetadata());
+
         String unlocalizedName = color.getTranslationKey();
-        // hotfix for compatibility with MoreChickens
-        if (unlocalizedName.equals("silver")) {
-            unlocalizedName += "Dye";
-        }
-        return I18n.translateToLocal(getTranslationKey() + "." + unlocalizedName + ".name");
+
+        String finalUnlocalizedName = unlocalizedName;
+
+        return TextComponentUtils.toTextComponent(() ->I18n.format(getTranslationKey() + "." + finalUnlocalizedName + ".name"));
     }
 
     @Override
@@ -74,13 +78,14 @@ public class ItemColoredEgg extends EggItem implements IColorSource {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand hand) {
+        Random itemRand = new Random();
         ItemStack itemStackIn = playerIn.getHeldItem(hand);
 
-        if (!playerIn.capabilities.isCreativeMode) {
+        if (!playerIn.isCreative()) {
             itemStackIn.shrink(1);
         }
 
-        worldIn.playSound(null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_EGG_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+        worldIn.playSound(playerIn, playerIn.getPosition(), SoundEvents.ENTITY_EGG_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
 
         if (!worldIn.isRemote) {
             String chickenType = getChickenType(itemStackIn);
@@ -88,13 +93,11 @@ public class ItemColoredEgg extends EggItem implements IColorSource {
                 EntityColoredEgg entityIn = new EntityColoredEgg(worldIn, playerIn);
                 entityIn.shoot(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 1.0F);
                 entityIn.setChickenType(chickenType);
-                worldIn.spawnEntity(entityIn);
+                entityIn.getType().spawn(worldIn, null, null, entityIn.getPosition(), SpawnReason.MOB_SUMMONED, true, false);
             }
         }
 
-        //noinspection ConstantConditions
-        playerIn.addStat(Stats.getObjectUseStats(this));
-        return new ActionResult<ItemStack>(ActionResultType.SUCCESS, itemStackIn);
+        return new ActionResult<>(ActionResultType.SUCCESS, itemStackIn);
     }
 
     public String getChickenType(ItemStack itemStack) {
