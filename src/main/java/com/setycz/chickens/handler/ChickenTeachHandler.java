be49915de192da731b5530b600872a040767d13d
Raphael Zöllner <raphael.zoellner@outlook.com>
2020-03-27 02:38:55 +0100

    WIP Update ChickenTeachHandler

--- src/main/java/com/setycz/chickens/handler/ChickenTeachHandler.java
+++ src/main/java/com/setycz/chickens/handler/ChickenTeachHandler.java
@@ -4,12 +4,15 @@ import com.setycz.chickens.entity.EntityChickensChicken;
 import com.setycz.chickens.registry.ChickensRegistry;
 import com.setycz.chickens.registry.ChickensRegistryItem;
 
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 /**
  * Created by setyc on 21.03.2016.
@@ -18,7 +21,8 @@ public class ChickenTeachHandler {
 
     @SubscribeEvent
     public void handleInteraction(PlayerInteractEvent.EntityInteract event) {
-        ItemStack item = event.getEntityPlayer().getHeldItem(event.getHand());
+        PlayerEntity player = event.getPlayer();
+        ItemStack item = player.getHeldItem(event.getHand());
         if (item.isEmpty() || item.getItem() != Items.BOOK) {
             return;
         }
@@ -26,7 +30,7 @@ public class ChickenTeachHandler {
             return;
         }
 
-        World worldObj = event.getEntityPlayer().world;
+        World worldObj = player.world;
         if (worldObj.isRemote) {
             return;
         }
@@ -39,8 +43,12 @@ public class ChickenTeachHandler {
         ChickenEntity chicken = (ChickenEntity) event.getTarget();
         EntityChickensChicken smartChicken = convertToSmart(chicken, worldObj, smartChickenDescription);
 
-        worldObj.removeEntity(chicken);
-        worldObj.spawnEntity(smartChicken);
+        BlockPos blockPos = chicken.getPosition();
+
+        chicken.remove();
+        chicken.onRemovedFromWorld();
+
+        smartChicken.getType().spawn(worldObj, null, player, blockPos, SpawnReason.CONVERSION, true, false);
         smartChicken.spawnExplosionParticle();
 
         event.setCanceled(true);
@@ -48,14 +56,16 @@ public class ChickenTeachHandler {
 
     private EntityChickensChicken convertToSmart(ChickenEntity chicken, World worldObj, ChickensRegistryItem smartChickenDescription) {
         EntityChickensChicken smartChicken = new EntityChickensChicken(worldObj);
-        smartChicken.setPositionAndRotation(chicken.posX, chicken.posY, chicken.posZ, chicken.rotationYaw, chicken.rotationPitch);
+        BlockPos chickenPos = chicken.getPosition();
+
+        smartChicken.setPositionAndRotation(chickenPos.getX(), chickenPos.getY(), chickenPos.getZ(), chicken.rotationYaw, chicken.rotationPitch);
         smartChicken.onInitialSpawn(worldObj.getDifficultyForLocation(chicken.getPosition()), null);
-        //smartChicken.setChickenType(smartChickenDescription.getId());
         smartChicken.setChickenType(ChickensRegistry.SMART_CHICKEN_ID.toString());
         
         if (chicken.hasCustomName()) {
-            smartChicken.setCustomNameTag(chicken.getCustomNameTag());
+            smartChicken.setCustomName(chicken.getCustomName());
         }
+
         smartChicken.setGrowingAge(chicken.getGrowingAge());
         return smartChicken;
     }
